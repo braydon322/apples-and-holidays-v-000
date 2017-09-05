4cdc722ebd32e8df4efc01e3e13fd657e1f0b4a8
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-05 21:14:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,9 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
         individual_holiday = holiday.to_s.split("_")
               individual_holiday.each do |word|
                 new_array.push(word.capitalize)
-                final_holiday = new_array.to_s
-                puts "#{final_holiday.strip}: #{items.flatten}"
-
+                new_array.push(items)
+                return new_array
               end
         end
 
